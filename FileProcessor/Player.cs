73b6ee04917dc650de29301e9be9a0b37c984c3b
Zach Sexton <ztsexton92@gmail.com>
2020-04-22 14:16:11 -0400

    Add CsvReader and display records sorted by a field

---
+++ FileProcessor/Player.cs
@@ -0,0 +1,39 @@
+﻿using CsvHelper.Configuration.Attributes;
+
+namespace FileProcessor
+{
+    public class Player
+    {
+        private int? Id { get; set; }
+        [Name("Player")]
+        public string PlayerName { get; set; }
+        public int? Year { get; set; }
+        [Name("Draft pick")]
+        public int? DraftPick { get; set; }
+        [Name("Height (No Shoes)")]
+        public double? Height { get; set; }
+        [Name("Height (With Shoes)")]
+        public double? HeightWithShoes { get; set; }
+        public double? Wingspan { get; set; }
+        [Name("Standing reach")]
+        public double? StandingReach { get; set; }
+        [Name("Vertical (Max)")]
+        public double? Vertical { get; set; }
+        [Name("Vertical (Max Reach)")]
+        public double? VerticalMaxReach { get; set; }
+        [Name("Vertical (No Step)")]
+        public double? VerticalNoStep { get; set; }
+        [Name("Vertical (No Step Reach)")]
+        public double? VerticalNoStepReach { get; set; }
+        public double? Weight { get; set; }
+        [Name("Body Fat")]
+        public double? BodyFat { get; set; }
+        [Name("Hand (Length)")]
+        public double? HandLength { get; set; }
+        [Name("Hand (Width)")]
+        public double? HandWidth { get; set; }
+        public double? Bench { get; set; }
+        public double? Agility { get; set; }
+        public double? Sprint { get; set; }
+    }
+}
---
+++ FileProcessor/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using CsvHelper;
+using CsvHelper.Configuration;
+using CsvHelper.TypeConversion;
+
+namespace FileProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var reader = new StreamReader("2012_nba_draft_combine.csv");
+            using var csvReader = new CsvReader(reader, CultureInfo.InvariantCulture);
+            var records = csvReader.GetRecords<Player>();
+            DisplayByDraftOrder(records);
+        }
+
+        private static void DisplayByDraftOrder(IEnumerable<Player> records)
+        {
+            records = records.OrderBy(r => r.DraftPick);
+            
+            foreach (var player in records)
+            {
+                Console.WriteLine($"{player.DraftPick}, {player.PlayerName}");
+            }
+        }
+    }
+}
